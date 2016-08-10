5e00f0c14c85db4c260cf77f089e5022e1bc9ca1
Tim Murphy <tim@26tp.com>
2016-08-11 03:53:56 +1000

    SAVEPOINT

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
-var fs = require('fs');
+var files = require('./scripts/files');
 var path = require('path');
 
 const msbuild = {
     configuration: 'Release',
     verbosity: 'Minimal'
 };
+
+const testsDirectory = path.join(__dirname, 'tests');
 
 const config = {
     clean: {
@@ -19,48 +21,12 @@ const config = {
     msbuild: msbuild,
     specflow: {
         cmd: `${__dirname}/packages/SpecFlow.2.1.0/tools/specflow.exe`,
-        projects: getSpecFlowProjects()
+        projects: files.getSpecFlowProjects(testsDirectory)
     },
     xunit: {
         cmd: `${__dirname}/packages/xunit.runner.console/tools/xunit.console.exe`,
-        assemblies: getTestAssemblies()
+        assemblies: files.getTestAssemblies(testsDirectory, msbuild.configuration)
     }
 };
-
-function getProject(directory) {
-    const files = fs.readdirSync(directory).filter(function (file) {
-        return path.extname(file) === '.csproj';
-    });
-
-    if (files.length === 1) {
-        return path.join(directory, files[0]);
-    }
-
-    throw `Expected to find 1 project in '${directory}', not ${files.length}.`;
-}
-
-function getSpecFlowDirectories() {
-    return getDirectories(path.join(__dirname, 'tests'))
-        .filter(function (directory) { return directory.endsWith('.Specifications') });
-}
-
-function getSpecFlowProjects() {
-    const directories = getSpecFlowDirectories();
-    const projects = directories.map(function (directory) { return getProject(directory); });
-
-    // projects.forEach(function(project) { console.log(project); });
-
-    return projects;
-}
-
-function getTestAssemblies() {
-    throw 'todo';
-}
-
-function getDirectories(srcpath) {
-    return fs.readdirSync(srcpath)
-        .filter(function (file) { return fs.statSync(path.join(srcpath, file)).isDirectory(); })
-        .map(function (file) { return path.join(srcpath, file); });
-}
 
 module.exports = config;
