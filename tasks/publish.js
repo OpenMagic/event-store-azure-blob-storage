c5c2efbf04e1153b73c021f7a3a0a669bc907c2c
Tim Murphy <tim@26tp.com>
2016-08-12 05:03:40 +1000

    SAVEPOINT

---
+++ tasks/publish.js
@@ -0,0 +1,30 @@
+const isGitClean = require('is-git-clean');
+
+module.exports = function (gulp, config, $) {
+    gulp.task('publish', function publish(cb) {
+        validateGitIsClean();
+
+        const newVersion = getNewVersionArgument(args);
+        const currentBranch = git.branch();
+
+        deleteGitBranchIfExists('publish')
+        createAndCheckoutGitBranch('publish');
+        updateVersion(newVersion);
+        createPackage();
+        mergePublishBranch(currentBranch);
+
+        // Push the latest commits and related tags to remote server
+        //shell.exec(`git push --follow-tags`);
+        pushRepository();
+
+        return cb();
+    });
+};
+
+// Check git repository is clean. Throws exception if it isn't.
+function validateGitIsClean() {
+    if (isGitClean.sync()) {
+        return;
+    }
+    throw 'Git repository must be clean before running the requested task.';
+}
