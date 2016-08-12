e92da39d6af043aad762e2389b8e069479aa8402
Tim Murphy <tim@26tp.com>
2016-08-13 08:50:46 +1000

    SAVEPOINT

--- tasks/version.js
+++ tasks/version.js
@@ -4,14 +4,14 @@ module.exports = function (gulp, config, $) {
     });
 
     gulp.task('version-npm-preversion', function publish(cb) {
-        $.log.info(`preversion: ${process.env.npm_package_version}`)
+        $.log.info(`npm-preversion: ${process.env.npm_package_version}`)
     });
 
     gulp.task('version-npm-version', function publish(cb) {
-        $.log.info(`version: ${process.env.npm_package_version}`)
+        $.log.info(`npm-version: ${process.env.npm_package_version}`)
     });
 
     gulp.task('version-npm-postversion', function publish(cb) {
-        $.log.info(`postversion: ${process.env.npm_package_version}`)
+        $.log.info(`npm-postversion: ${process.env.npm_package_version}`)
     });
 }
