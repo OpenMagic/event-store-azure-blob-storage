da50dc7b92a794e111834efaccdc375aef264a64
Tim Murphy <tim@26tp.com>
2016-08-13 08:56:59 +1000

    SAVEPOINT

--- tasks/version.js
+++ tasks/version.js
@@ -6,13 +6,16 @@ module.exports = function (gulp, config, $) {
 
     gulp.task('version-npm-preversion', function publish(cb) {
         $.log.info(`npm-preversion: ${process.env.npm_package_version}`)
+        cb();
     });
 
     gulp.task('version-npm-version', function publish(cb) {
         $.log.info(`npm-version: ${process.env.npm_package_version}`)
+        cb();
     });
 
     gulp.task('version-npm-postversion', function publish(cb) {
         $.log.info(`npm-postversion: ${process.env.npm_package_version}`)
+        cb();
     });
 }
